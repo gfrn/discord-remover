09ad0ad07203214c65fd5faed322775527587446
Guilherme Francisco de Freitas <guifreitas89@hotmail.com>
2020-04-04 09:28:21 -0300

    Add Canary removal

--- rm_discord.bat
+++ rm_discord.bat
@@ -20,7 +20,22 @@ GOTO :choice
 net session >nul 2>&1
 if not %errorLevel%== 0 (echo Run CMD/Script as Admin and try again ^(Left click ^> Run as administrator^) && pause && GOTO :eof)
 
-if "%1"=="s" (echo Skipping registry backup... && GOTO :skipreg)
+set isAll=F
+if "%1" == "a" set isAll=T
+if "%2" == "a" set isAll=T
+if "%isAll%"=="T" (echo Uninstalling all Discord instances...)
+
+set isCanary=F
+if "%1" == "c" set isCanary=T
+if "%2" == "c" set isCanary=T
+if "%isCanary%"=="T" if "%isAll%"=="F" (echo Uninstalling Discord Canary...)
+if "%isCanary%"=="T" if "%isAll%"=="T" (echo Uninstall all taking precedence over Canary...)
+
+set isSkip=F
+if "%1" == "s" set isSkip=T
+if "%2" == "s" set isSkip=T
+if "%isSkip%"=="T" (echo Skipping registry backup... && GOTO :skipreg)
+
 echo Creating registry backup...
 
 reg export HKLM hklm.reg > nul
@@ -32,14 +47,16 @@ reg export HKCC hkcc.reg > nul
 :skipreg
 echo Removing app data...
 
-rmdir /S /Q "%appdata%/discord"
-rmdir /S /Q "%localappdata%/Discord"
+if %isCanary%=="T" (rmdir /S /Q "%appdata%/discordcanary") else (rmdir /S /Q "%appdata%/discord")
+if %isCanary%=="T" (rmdir /S /Q "%localappdata%/DiscordCanary") else (rmdir /S /Q "%localappdata%/Discord")
 rmdir /S /Q "%localappdata%/SquirrelTemp"
 
 echo Removing registry entries...
 
-REG DELETE HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Run /v Discord /f
-REG DELETE HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run /v Discord /f
+if %isCanary%=="T" (REG DELETE HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Run /v DiscordCanary /f) else (REG DELETE HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Run /v Discord /f)
+if %isCanary%=="T" (REG DELETE HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run /v DiscordCanary /f) else (REG DELETE HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run /v Discord /f)
+
+if "%isAll%"=="T" (set isCanary="T" && set isAll="F" && GOTO :skipreg)
 
 echo Finished removing Discord leftovers! Have a nice day.
 endlocal
